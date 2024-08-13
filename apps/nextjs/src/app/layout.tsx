475a6da9dc43e72cf4e252dd9388672d3189ef7f
Ryan Waldon <ryanpwaldon@gmail.com>
2024-08-13 19:50:59 +1000

    feat: updates

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata, Viewport } from 'next'
-import { cn } from '@sctv/ui'
-import { ThemeProvider, ThemeToggle } from '@sctv/ui/theme'
-import { Toaster } from '@sctv/ui/toast'
 import { GeistMono } from 'geist/font/mono'
 import { GeistSans } from 'geist/font/sans'
+
+import { cn } from '@sctv/ui'
+import { ThemeProvider } from '@sctv/ui/theme'
 
 import { TRPCReactProvider } from '~/trpc/react'
 
@@ -12,20 +12,9 @@ import '~/app/globals.css'
 import { env } from '~/env'
 
 export const metadata: Metadata = {
-  metadataBase: new URL(env.VERCEL_ENV === 'production' ? 'https://turbo.t3.gg' : 'http://localhost:3000'),
-  title: 'Create T3 Turbo',
-  description: 'Simple monorepo with shared backend for web & mobile apps',
-  openGraph: {
-    title: 'Create T3 Turbo',
-    description: 'Simple monorepo with shared backend for web & mobile apps',
-    url: 'https://create-t3-turbo.vercel.app',
-    siteName: 'Create T3 Turbo',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    site: '@jullerino',
-    creator: '@jullerino',
-  },
+  metadataBase: new URL(env.VERCEL_ENV === 'production' ? 'https://sctv.com/' : 'http://localhost:3000'),
+  title: 'SCTV',
+  description: undefined,
 }
 
 export const viewport: Viewport = {
@@ -41,10 +30,6 @@ export default function RootLayout(props: { children: React.ReactNode }) {
       <body className={cn('min-h-screen bg-background font-sans text-foreground antialiased', GeistSans.variable, GeistMono.variable)}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <TRPCReactProvider>{props.children}</TRPCReactProvider>
-          <div className="absolute bottom-4 right-4">
-            <ThemeToggle />
-          </div>
-          <Toaster />
         </ThemeProvider>
       </body>
     </html>
---
+++ apps/nextjs/src/components/mapbox.tsx
@@ -0,0 +1,5 @@
+'use client'
+
+export const Mapbox = () => {
+  return <div className="h-full w-full"></div>
+}
