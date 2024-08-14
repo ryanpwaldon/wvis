1875882e63dba64f64a36b3d2f78db11212950fe
Ryan Waldon <ryanpwaldon@gmail.com>
2024-08-14 15:03:30 +1000

    feat: updates

--- apps/nextjs/src/components/mapbox.tsx
+++ apps/nextjs/src/components/mapbox.tsx
@@ -1,18 +1,29 @@
 'use client'
 
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import useMap from '~/hooks/useMap'
 
 export const Mapbox = () => {
+  const [mapInit, setMapInit] = useState(false)
   const mapContainerRef = useRef<HTMLDivElement>(null)
-  useMap({
+  const { map } = useMap({
     mapContainerRef,
     options: {
       center: [-74.5, 40],
       zoom: 2,
     },
   })
+
+  useEffect(() => {
+    if (map && !mapInit) {
+      setMapInit(true)
+      map.once('load', () => {
+        return
+      })
+    }
+  }, [map, mapInit])
+
   return (
     <div className="relative h-full w-full bg-background [&_.mapboxgl-map_.mapboxgl-ctrl-logo]:!hidden [&_canvas]:!outline-none">
       <div ref={mapContainerRef} className="h-full w-full" />
