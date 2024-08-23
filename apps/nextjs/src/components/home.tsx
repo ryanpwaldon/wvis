a7753b254b1c36ec15e514d71c95893df4ea65a9
Ryan Waldon <ryanpwaldon@gmail.com>
2024-08-23 11:37:06 +1000

    feat: updates

--- apps/nextjs/src/components/home.tsx
+++ apps/nextjs/src/components/home.tsx
@@ -3,6 +3,7 @@
 import { useMemo, useState } from 'react'
 
 import { CLOUDFLARE_BUCKET_URL } from '@sctv/constants'
+import { ThemeToggle } from '@sctv/ui/theme'
 
 import { Mapbox } from '~/components/mapbox'
 import useImageData from '~/hooks/useImageData'
@@ -17,14 +18,17 @@ export const Home = () => {
 
   return (
     <div className="h-full w-full border p-4">
-      <div className="relative h-full w-full overflow-hidden">
+      <div className="flex h-full w-full flex-col divide-y overflow-hidden border">
         <Mapbox>
           <MapboxParticleLayer imageData={imageData} />
           <MapboxChoroplethLayer imageData={imageData} />
         </Mapbox>
-        <div className="absolute bottom-0 left-0 w-full p-4">
-          <Timeline onChange={setSelectedDate} interval={3} days={7} />
+        <div className="flex h-8 w-full justify-end bg-card text-card-foreground">
+          <ThemeToggle />
         </div>
+      </div>
+      <div className="hidden">
+        <Timeline onChange={setSelectedDate} interval={3} days={7} />
       </div>
     </div>
   )
