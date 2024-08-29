41b2c57f151877e506b77fd16300493dcba7412c
Ryan Waldon <ryanpwaldon@gmail.com>
2024-08-29 18:23:12 +1000

    fix: bug

--- apps/weather/src/utils/generateFlowFieldImage.ts
+++ apps/weather/src/utils/generateFlowFieldImage.ts
@@ -1,4 +1,4 @@
-import jimp from 'jimp'
+import Jimp from 'jimp'
 
 interface GenerateFlowFieldImageProps {
   uValues: number[]
@@ -11,7 +11,8 @@ interface GenerateFlowFieldImageProps {
 
 export const generateFlowFieldImage = async ({ uValues, vValues, width, height, minMagnitude, maxMagnitude }: GenerateFlowFieldImageProps): Promise<Buffer> => {
   if (uValues.length !== width * height || vValues.length !== width * height) throw new Error("Array size doesn't match the provided dimensions.")
-  const image = new jimp(width, height, 0x00000000)
+  if (minMagnitude >= maxMagnitude) throw new Error('minMagnitude must be less than maxMagnitude.')
+  const image = new Jimp(width, height, 0x00000000)
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
       const adjustedY = height - y - 1 // invert y-axis
@@ -20,15 +21,15 @@ export const generateFlowFieldImage = async ({ uValues, vValues, width, height, 
       const v = vValues[index]
       if (u === undefined || v === undefined) throw new Error(`Undefined value encountered at index ${index}`)
       // Clamp U and V to the min/max range
-      const clampedUgrdValue = Math.max(minMagnitude, Math.min(maxMagnitude, u))
-      const clampedVgrdValue = Math.max(minMagnitude, Math.min(maxMagnitude, v))
-      // Normalize clamped values to the 0-255 range
-      const red = Math.floor(((clampedUgrdValue + 100) / 200) * 255)
-      const green = Math.floor(((clampedVgrdValue + 100) / 200) * 255)
+      const clampedU = Math.max(minMagnitude, Math.min(maxMagnitude, u))
+      const clampedV = Math.max(minMagnitude, Math.min(maxMagnitude, v))
+      // Normalize clamped values based on the min/max range provided
+      const red = Math.floor(((clampedU - minMagnitude) / (maxMagnitude - minMagnitude)) * 255)
+      const green = Math.floor(((clampedV - minMagnitude) / (maxMagnitude - minMagnitude)) * 255)
       // Combine red and green values into one pixel
-      const color = jimp.rgbaToInt(red, green, 0, 255)
+      const color = Jimp.rgbaToInt(red, green, 0, 255)
       image.setPixelColor(color, x, adjustedY)
     }
   }
-  return await image.getBufferAsync(jimp.MIME_PNG)
+  return await image.getBufferAsync(Jimp.MIME_PNG)
 }
