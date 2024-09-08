ad00f6106b83b443e7f8ffdaf8e5bca0bbf6af93
Ryan Waldon <ryanpwaldon@gmail.com>
2024-09-08 17:09:45 +1000

    feat: add vector grid caching

--- apps/nextjs/src/hooks/useVectorGridData.ts
+++ apps/nextjs/src/hooks/useVectorGridData.ts
@@ -20,6 +20,9 @@ interface UseVectorGridDataProps {
   config: VectorGridConfig
 }
 
+// Create a cache to store processed image data
+const cache = new Map<string, VectorGrid>()
+
 export const useVectorGridData = ({ date, config }: UseVectorGridDataProps) => {
   const [vectorGrid, setVectorGrid] = useState<VectorGrid | null>(null)
   const [isLoading, setIsLoading] = useState<boolean>(false)
@@ -28,14 +31,32 @@ export const useVectorGridData = ({ date, config }: UseVectorGridDataProps) => {
 
   const loadImage = useCallback(
     async (date: Date) => {
+      const url = config.url(date)
+
+      // Check if the data for this URL is already in the cache
+      if (cache.has(url)) {
+        const cachedData = cache.get(url)!
+        setVectorGrid(cachedData)
+        setIsLoading(false)
+        return
+      }
+
       try {
-        const url = config.url(date)
         const arrayBuffer = await ky(url, { mode: 'cors' }).arrayBuffer()
-        const decoded = await decodePng(new Uint8Array(arrayBuffer), { parseChunkTypes: '*', strictMode: true })
         if (date === latestDate.current) {
+          const decoded = await decodePng(new Uint8Array(arrayBuffer), { parseChunkTypes: '*', strictMode: true })
           const decodedImage = new ImageData(new Uint8ClampedArray(decoded.image.data.buffer), decoded.image.width, decoded.image.height)
-          const decodedMetadata = vectorGridMetadataSchema.parse(Object.fromEntries(decoded.metadata.filter((item): item is IPngMetadataTextualData => item.type === 'tEXt').map((item) => [item.keyword, item.text]))) // prettier-ignore
-          setVectorGrid({ image: decodedImage, metadata: decodedMetadata, config })
+          const decodedMetadata = vectorGridMetadataSchema.parse(
+            Object.fromEntries(
+              decoded.metadata.filter((item): item is IPngMetadataTextualData => item.type === 'tEXt').map((item) => [item.keyword, item.text]),
+            ),
+          )
+          const newVectorGrid = { image: decodedImage, metadata: decodedMetadata, config }
+
+          // Store the processed data in the cache using the URL as the key
+          cache.set(url, newVectorGrid)
+
+          setVectorGrid(newVectorGrid)
           setIsLoading(false)
           setError(null)
         }
@@ -61,7 +82,9 @@ export const useVectorGridData = ({ date, config }: UseVectorGridDataProps) => {
     }
   }, [date, loadImage])
 
-  useEffect(() => void (latestDate.current = date), [date])
+  useEffect(() => {
+    latestDate.current = date
+  }, [date])
 
   return { vectorGrid, isLoading, error }
 }
